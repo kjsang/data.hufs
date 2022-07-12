de687b3d89fbf9bab1ef5d843668f954dfcc059b
Kim, Jusang <75797388+kjsang@users.noreply.github.com>
2022-07-12 17:48:40 +0900

    1

--- 1.EDA/EDA.R
+++ 1.EDA/EDA.R
@@ -6,7 +6,9 @@ pacman::p_load(
   rstatix, # 통계툴
   palmerpenguins, # 데이터셋
   RColorBrewer, 
-  naniar # useful for visualising missing values
+  naniar, # useful for visualising missing values
+  PerformanceAnalytics, # 왜도 및 첨도
+  psych # 기하평균, 조화평균 구하기
   )
 
 
@@ -19,10 +21,62 @@ data %>% summary() # 각 변수별로 NA 값이 얼마나 포함되어 있는지
 
 
 # 2.1. data preprocessing -------------------------------------------------
-data %>% naniar::vis_miss() # NA 시각화해보니 별로 없다. 그냥 지우자.
+data %>% vis_miss() # NA 시각화해보니 별로 없다. 그냥 지우자.
 data %>% na.omit() -> data_prep
 data_prep %>% vis_miss() # 깔끔하다.
 
+
+# 2.2. 데이터 특성 파악: 왜도 및 첨도 -------------------------------------------------
+data_prep %>% 
+  filter(species == "Adelie") %>% 
+  summarise(
+    왜도 = PerformanceAnalytics::skewness(bill_length_mm),
+    첨도 = PerformanceAnalytics::kurtosis(bill_length_mm)
+  )
+# 살짝 평평하고 살짝 왼쪽으로 치우친 분포
+
+data_prep %>% 
+  filter(species == "Adelie") %>% 
+  get_summary_stats(bill_length_mm)
+
+# n: the number of individuals
+# min: minimum
+# max: maximum
+# median: median
+# mean: mean
+# q1, q3: the first and the third quartile, respectively.
+# iqr: interquartile range
+# mad: median absolute deviation (see ?MAD)
+# sd: standard deviation of the mean
+# se: standard error of the mean
+# ci: 95 percent confidence interval of the mean
+
+# CV 구하는 방법: sd / mean * 100
+data_prep %>% 
+  filter(species == "Adelie") %>% 
+  summarise(
+    CV = sd(bill_length_mm)/mean(bill_length_mm)*100
+  )
+
+
+
+data_prep %>% 
+  filter(species == "Adelie") %>% 
+  ggplot(aes(bill_length_mm)) +
+  geom_histogram(alpha = 1, colour = "cornflowerblue",
+                 bins = 20) +
+  annotate("segment", x = 38.8, xend = 38.8, y = 0, yend = 18,
+           colour = "red", size = 2) + # 평균
+  annotate("segment", x = 38.8, xend = 38.8, y = 0, yend = 18,
+           colour = "blue", size = 1) # 중앙값
+#평균과 중앙값이 같으나, 분포는 다를 수 있고, 이에 따라 왜도가 차이가 날 수 있다.
+
+data_prep %>% 
+  filter(species == "Adelie") %>% 
+  ggplot(aes(x = species, y = bill_length_mm)) +
+  geom_boxplot(aes(color = species),
+               width = 0.3) + 
+  coord_flip() # 왼쪽에 데이터가 더 많다.
 
 # 3. Visualization --------------------------------------------------------
 
@@ -66,6 +120,7 @@ data_prep2 %>%
   xlab("날개길이") +
   ylab("빈도")
 
+
 # 3.3. 박스플롯 ---------------------------------------------------------------
 data_prep2 %>% 
   ggplot(aes(x = species, y = flipper_length_mm)) +
@@ -78,3 +133,102 @@ data_prep2 %>%
   xlab("종") +
   ylab("날개길이")
 
+
+# 4. 상관관계분석 ---------------------------------------------------------------
+
+data_cor <- mtcars
+data_cor %>% glimpse()
+
+# [, 1]	mpg	Miles/(US) gallon -> Interval
+# [, 2]	cyl	Number of cylinders 
+# [, 3]	disp	Displacement (cu.in.)
+# [, 4]	hp	Gross horsepower
+# [, 5]	drat	Rear axle ratio
+# [, 6]	wt	Weight (1000 lbs)
+# [, 7]	qsec	1/4 mile time
+# [, 8]	vs	Engine (0 = V-shaped, 1 = straight) 
+# [, 9]	am	Transmission (0 = automatic, 1 = manual)
+# [,10]	gear	Number of forward gears
+
+
+# 4.1. 피어슨 상관분석 -----------------------------------------------------------
+data_cor %>% 
+  rstatix::cor_test(vars = mpg, vars2 = disp,
+                    alternative = "two.sided",
+                    method = "pearson",
+                    conf.level = 0.95)
+
+# 4.2. 스피어만 상관분석: 순서 대 순서 ----------------------------------------------------------
+data_cor %>% 
+  rstatix::cor_test(vars = cyl, vars2 = gear,
+                    alternative = "two.sided",
+                    method = "spearman",
+                    conf.level = 0.95)
+
+# 4.3. 켄달타우: 순서 대 순서 ---------------------------------------------------------------
+data_cor %>% 
+  rstatix::cor_test(vars = cyl, vars2 = gear,
+                    alternative = "two.sided",
+                    method = "kendall",
+                    conf.level = 0.95)
+
+
+# 4.4. 점이연 상관계수: 연속 대 명목 --------------------------------------------------
+data_cor %>% 
+  rstatix::cor_test(vars = mpg, vars2 = am,
+                    alternative = "two.sided",
+                    method = "pearson", # 점이연은 피어슨과 계산이 같다
+                    conf.level = 0.95)
+
+# 4.5. 이연 상관계수: 연속 대 명목(원래는 연속이었던) --------------------------------------------------
+data_cor %>% 
+  get_summary_stats(mpg) # 평균이 20이니, 20이상은 1, 20이하는 0으로 코딩
+
+data_cor %>% 
+  mutate(mpg_prep = ifelse(mpg >= 20, 1, 0)) %>% # 20 이상은 1, 20이하는 0으로 코딩
+  rstatix::cor_test(vars = mpg_prep, vars2 = disp,
+                    alternative = "two.sided",
+                    method = "pearson", # 이연은 피어슨과 계산이 같다
+                    conf.level = 0.95)
+
+# 4.6. 파이 상관계수: 명목 대 명목 --------------------------------------------------
+data_cor %>% 
+  rstatix::cor_test(vars = vs, vars2 = am,
+                    alternative = "two.sided",
+                    method = "pearson", # 파이는 피어슨과 계산이 같다
+                    conf.level = 0.95)
+
+#### 기타 ####
+# Tetrachoric 상관계수– 범주들간의 상관계수이나, 범주들이 인위적으로 이분화된 경우에 사용하는 이다.– 이분화되기 전 원래 변수는 정규분포를 띠고 있다고 가정한다.
+
+
+# 5. 평균 -------------------------------------------------------------------
+read_csv("0.data\\1.Korea_GDP.csv") -> GDP
+GDP %>% 
+  pivot_longer(cols = `1970`:`2021`,
+               names_to = "Year",
+               values_to = "GDP") -> GDP_prep # 데이터 적재
+
+# 5.2. 기하평균 & 조화평균 ---------------------------------------------------------------
+GDP_prep %>% 
+  summarise(
+    H = harmonic.mean(GDP), # 31372
+    M = mean(GDP), # 665664.
+    G = geometric.mean(GDP) # 230684.
+  ) # 순서는? M > G > H
+
+GDP_prep %>% 
+  arrange(Year) %>% 
+  mutate(Year = as.integer(Year),
+         Diff_year = Year - lag(Year),
+         Diff_growth = GDP - lag(GDP),
+         Rate_percent = (Diff_growth / Diff_year)/GDP*100) -> GDP_growth
+GDP_growth %>% 
+  filter(!Year == 1970) %>% 
+  summarise(
+    G = geometric.mean(Rate_percent) # 평균 GDP 증가율
+  )
+
+# 내가 산에 올라갈 때에는 시속 6킬로, 내려올 때에는 시속 12킬로로 왔다면 나의 평균 속력은?
+mountain <- c(6, 12)
+harmonic.mean(mountain) # 평균시속 8 (평균 9가 아니다)
